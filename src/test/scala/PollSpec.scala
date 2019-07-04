be1fe56fb2a40f6c9d09be938411e3a2c976023c
tampler <socnetfpga@gmail.com>
2019-07-04 16:13:54 +0300

    Updated poll test

--- src/test/scala/PollSpec.scala
+++ src/test/scala/PollSpec.scala
@@ -6,12 +6,14 @@ import zio.{ Chunk, DefaultRuntime, IO, UIO, ZIO }
 
 import net.manub.embeddedkafka.EmbeddedKafka
 import kafkaconsumer._
+import kafkaconsumer.KafkaConsumer.KafkaData
 import zio.kafka.client.KafkaTestUtils.{ produceMany }
 
 class PollSpec extends Specification with DefaultRuntime {
-  EmbeddedKafka.start()
+  // val kafka = EmbeddedKafka.start()
+  //EmbeddedKafka.publishStringMessageToKafka()
 
-  type KafkaData = List[(String, String)]
+  type KafkaDataOld = List[(String, String)]
 
   // number of messages to produce
   val msgCount   = 2
@@ -20,25 +22,31 @@ class PollSpec extends Specification with DefaultRuntime {
   def genPortRange(start: Int, end: Int): Int =
     start + scala.util.Random.nextInt((end - start) + 1)
 
-  def genData: KafkaData =
+  def genData: KafkaDataOld =
     (1 to msgCount).toList.map(i => (s"key$i", s"msg$i"))
 
   def buildVirtualServer: String = {
 
-    val port = genPortRange(9090, 9200)
-    println(s"Starting Kafka Server on port $port")
-    val bootstrapServer = s"localhost:$port"
+    val kafka = EmbeddedKafka.start()
+    // val port = genPortRange(9090, 9200)
+    //val port = "6001"
+    //println(s"Starting Kafka Server on port $port")
+    //val bootstrapServer = s"localhost:$port"
+    val bootstrapServer = s"localhost:${kafka.config.kafkaPort}"
+
     bootstrapServer
   }
 
   def is = s2"""
 
   TSP Kafka should
-    publish data to a topic                     $t0        
-    poll and peek                               $t2  
-    
+    produce and consume                         $t4
     shutdown all                                $shutdown  
+
     """
+  // publish data to a topic                     $t0
+  // poll and peek                               $t2
+
   // subscribe for a topic                       $t1
   // poll and read                               $t3
 
@@ -93,9 +101,15 @@ class PollSpec extends Specification with DefaultRuntime {
       topic = "testTopic"
     )
 
-    val data: Chunk[String] = KafkaConsumer.peekBatch(cfg)    
+    //val data: Chunk[String] = KafkaConsumer.peekBatch(cfg)
+    val data =
+      for {
+        _   <- KafkaConsumer.produce(cfg)
+        out = KafkaConsumer.peekBatch(cfg)
 
-    data must_== Chunk.fromIterable(genData)    
+      } yield out
+
+    data must_== Chunk.fromIterable(genData)
   }
 
   def t3 = {
@@ -110,6 +124,24 @@ class PollSpec extends Specification with DefaultRuntime {
     val res = KafkaConsumer.readBatch(cfg)
 
     res.isEmpty must_== false
+  }
+
+  def t4() = {
+
+    val cfg = ConnectionConfig(
+      server = buildVirtualServer,
+      client = "client0",
+      group = "group0",
+      topic = "testTopic"
+    )
+
+    val data: KafkaData = KafkaConsumer.produceAndConsume(cfg)
+
+    // Validate output
+    data.map { r =>
+      (r.key, r.value)
+    } must_== Chunk.fromIterable(genData)
+
   }
 
   def shutdown() = {
