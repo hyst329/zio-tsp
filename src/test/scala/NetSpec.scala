88e1480e81e239c643cd988f35fa5d88dc76a924
tampler <socnetfpga@gmail.com>
2019-07-06 20:43:20 +0300

    Updated net spec

--- src/test/scala/NetSpec.scala
+++ src/test/scala/NetSpec.scala
@@ -19,6 +19,15 @@ import ParquetReader._
 
 class NetSpec extends Specification with DefaultRuntime {
 
+  val path = "/tmp/hello.pq"
+
+  val rows: UIO[(Int, TypeData)] =
+    for {
+      frame <- Reader.getFrame(path)
+      size  = frame.getRowSize
+      data  <- Reader.getRows(frame)
+    } yield (size, data)
+
   // number of partitions
   val partNumber = 1
 
@@ -39,25 +48,38 @@ class NetSpec extends Specification with DefaultRuntime {
   def is = s2"""
 
   TSP Network should      
-    hello     $hello
+    display parquet file contents     $disp
+    publish parquet file to Kafka     $pub
 
     """
 
-  def hello = {
+  def disp = {
 
-    val path = "/tmp/hello.pq"
-
-    val rows =
-      for {
-        frame <- Reader.getFrame(path)
-        size  = frame.getRowSize
-        data  <- Reader.getRows(frame)
-      } yield (size, data)
-
-    // rows.dd
     unsafeRun(rows.map(println))
-
     true must_== true
+  }
+
+  def pub = {
+
+    val subscription = Subscription.Topics(Set(cfg.topic))
+
+    val cons = Consumer.make[String, String](settings(cfg))
+
+    val res: BlockingTask[Chunk[(String, String)]] = cons.use { r =>
+      for {
+
+        _    <- r.subscribe(subscription)
+        _    <- produceMany[List, (String, String)](cfg.topic, genDummyData)
+        data <- pollNtimes(5, r)
+
+      } yield data.map { r =>
+        (r.key, r.value)
+      }
+    }
+
+    unsafeRun(res) must_== Chunk.fromIterable(genDummyData)
+
+    //true must_== true
   }
 
 }
