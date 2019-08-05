46e34c7c060bf4061067922077bbe05ea4b9f301
tampler <socnetfpga@gmail.com>
2019-08-05 11:11:44 +0300

    Started to impl Arrow Producer

--- src/test/scala/Arrow.scala
+++ src/test/scala/Arrow.scala
@@ -2,29 +2,27 @@ package nettest
 
 import org.specs2._
 
-import java.io.{ ByteArrayInputStream }
+import java.io.{ ByteArrayInputStream, ByteArrayOutputStream }
 import java.util.Collections
 import java.util.Arrays.asList
 
-import zio.{ DefaultRuntime }
+import zio.{ Chunk, DefaultRuntime }
 // import zio.console.{ putStrLn }
 
-import net.manub.embeddedkafka.{ EmbeddedKafka }
-import kafkaconsumer.KafkaConsumer._
+import org.apache.kafka.common.serialization.Serdes
+import net.manub.embeddedkafka.{ EmbeddedKafka, EmbeddedKafkaConfig }
 import zio.kafka.client.KafkaTestUtils.{ pollNtimes }
+import zio.kafka.client.{ Consumer, Subscription }
 import KafkaPkg._
 import KafkaTypes._
-import zio.kafka.client.{ Consumer, Subscription }
-import zio.kafka.client._
-
-import org.apache.kafka.common.serialization.Serdes
+import kafkaconsumer.KafkaConsumer.{ settings }
 
 import org.apache.arrow.memory.RootAllocator
-import org.apache.arrow.vector.ipc.{ ArrowStreamReader }
+import org.apache.arrow.vector.ipc.{ ArrowStreamReader, ArrowStreamWriter }
 import org.apache.arrow.vector.{ IntVector, VectorSchemaRoot }
 import org.apache.arrow.vector.types.pojo.{ ArrowType, Field, FieldType, Schema }
 
-import zio.{ Chunk }
+import zio.serdes.Serdes._
 
 class ArrowSpec extends Specification with DefaultRuntime {
 
@@ -35,9 +33,10 @@ class ArrowSpec extends Specification with DefaultRuntime {
   TSP Arrow should      
     display parquet file contents     
 
-    consume parquet from prod       
-    consume arrow from prod         $testConsumeArrow
-    killall                         $killall
+    consume parquet from prod      
+    consume arrow from prod                 $testConsumeArrow
+    produce and consume arrow locally       $testProduceAndConsumeArrow
+    killall                                 $killall
 
     """
   def testConsumeParquet = {
@@ -91,7 +90,8 @@ class ArrowSpec extends Specification with DefaultRuntime {
           batch <- pollNtimes(5, r)
           _     <- r.unsubscribe
 
-          arr    = batch.map(_.value)
+          arr = batch.map(_.value)
+          // _      <- putStrLn(arr.toString)
           reader = deserialize(arr)
           schema = reader.map(r => r.getVectorSchemaRoot.getSchema)
           empty  = reader.map(r => r.loadNextBatch)
@@ -103,12 +103,51 @@ class ArrowSpec extends Specification with DefaultRuntime {
 
   }
 
+  def testProduceAndConsumeArrow = {
+
+    val netCfg = NetConfig(
+      kafkaPort = 9003,
+      zooPort = 6001
+    )
+
+    val slvCfg = SlaveConfig(
+      server = s"localhost:${netCfg.kafkaPort}",
+      client = "client1",
+      group = "group1",
+      topic = "BArrTopic"
+    )
+
+    val cfg = EmbeddedKafkaConfig(kafkaPort = netCfg.kafkaPort, zooKeeperPort = netCfg.zooPort)
+    // EmbeddedKafka.start()(cfg)
+
+    // val data: BArr   = Array(1, 2, 3)
+    // val data   = Chunk(1, 2, 3)
+    // val stream = scatter(data)
+    val root = simpleRoot(testSchema)
+    root.getFieldVectors.get(0).allocateNew
+    // val vec: TinyIntVector = root.getFieldVectors.get(0).asInstanceOf[TinyIntVector]
+
+    val out = new ByteArrayOutputStream
+
+    val writer = new ArrowStreamWriter(root, null, out)
+    writer.close
+
+    out.size must be > 0
+
+  }
+
   def deserialize(din: Chunk[BArr]): Chunk[ArrowStreamReader] =
     for {
       arr    <- din
       stream = new ByteArrayInputStream(arr)
 
     } yield new ArrowStreamReader(stream, allocator)
+
+  def serialize(din: Chunk[Int]) = {
+    val stream = scatter(din)
+    // val writer = new ArrowStreamWriter (allocator, )
+
+  }
 
   def testSchema = {
     val schema = new Schema(
